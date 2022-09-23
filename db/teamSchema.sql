23b909afaf565c158a2265ca7f13dd2049c33772
capg84 <cyprian.gomes@live.co.uk>
2022-09-23 15:47:06 +0100

    updated teamSchema.sql to fix errors

--- db/teamSchema.sql
+++ db/teamSchema.sql
@@ -19,7 +19,7 @@ CREATE TABLE roles (
     id INT AUTO_INCREMENT PRIMARY KEY,
     role_name VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
-    department_id INT NOT NULL,
+    department_id INT,
     FOREIGN KEY (department_id)
     REFERENCES departments(id)
     ON DELETE SET NULL
@@ -30,7 +30,7 @@ CREATE TABLE employees (
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id)
     REFERENCES roles(id)
