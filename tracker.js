596fd76f8b70833dcfe326b66bf135e55b1c85a0
capg84 <cyprian.gomes@live.co.uk>
2022-09-25 21:52:56 +0100

    fixed db connection on tracker.js

--- tracker.js
+++ tracker.js
@@ -3,103 +3,129 @@ const inquirer = require("inquirer");
 const mysql = require("mysql2");
 const consoleTable = require("console.table");
 const asciiArtLogo = require("asciiart-logo");
-const dbConnection = require("./config/connection");
 
-// const PORT = process.env.PORT || 3001;
-
-//   dbConnection.connect(function(err) {
-//     if (err) throw err;
-   
-//     console.log("Connected as ID " + dbConnection.threadId);
-//     console.clear();
-//     console.log ("============================================");
-//     console.log ("");
-//     console.log ("   WELCOME TO ONE TEAM EMPLOYEE DATABASE   ");
-//     console.log ("");
-//     console.log ("============================================");
-//     manageOneTeam();
-//   }),
-// );
+// creating sql database connection
+const dbConnection = mysql.createConnection(
+    {
+      host: 'localhost',
+      user: 'root',
+      password: 'none093rare977',
+      database: 'one_team_employee_db'
+    },
+    
+    console.log ("============================================"),
+    console.log (""),
+    console.log ("   WELCOME TO ONE TEAM EMPLOYEE DATABASE   "),
+    console.log (""),
+    console.log ("============================================"),
+    manageOneTeam()
+);
 
 // creating manageOneTeam function
 function manageOneTeam() {
-// prompting the options for the user
-inquirer.prompt ([
-    {
-    type: "list",
-    massage: "Please select one option",
-    name: "action",
-    choices: [
-        "View all departments",
-        "View all roles",
-        "View employees",
-        "Add a department",
-        "Add a role",
-        "Add an employee",
-        "Update an employee information",
-        "View departmental spend on HR",
-        "Delete a department, role or employee",
-        "Exit One Team Manager Application"
-        ]
-    }
-    ]).then(function(selection){
-        // creating switch based on the selected action
-        switch(selection.action) {
-            // viewAllDepartment function if "View all department" is selected
-            case "View all departments": 
-                viewAllDepartments();
-            break;
-
-            // viewAllRoles function if "View all role" is selected
-            case "View all roles":
-                viewAllRoles();
-            break;
-
-            // viewEmployees function if "View employees" is selected
-            case "View employees":
-                viewEmployees();
-            break;
-
-            // departmentAdd function if "Add a department" is selected
-            case "Add a department":
-                departmentAdd();
-            break;
-
-            // roleAdd function if "Add a role" is selected
-            case "Add a role":
-                roleAdd();
-            break;
-
-            // employeeAdd function if "Add an employee" is selected
-            case "Add an employee":
-                employeeAdd();
-            break;
-
-            // updateEmployee function if "Update an employee information" is selected
-            case "Update an employee information":
-                updateEmployee();
-            break;
-
-            // viewSpend function if "View departmental spend on HR" is selected
-            case "View departmental spend on HR":
-                viewSpend();
-            break;
-
-            // deleteInfo function if "Delete a department, role or employee" is selected
-            case "Delete a department, role or employee":
-                deleteInfo();
-            break;
-
-            // exit from the application
-            case "Exit One Team Manager Application":
-                console.log ("===========================================================");
-                console.log ("");
-                console.log ("   YOU HAVE SUCCESSFULLY LOGGED OUT FROM THE APPLICATION   ");
-                console.log ("");
-                console.log ("===========================================================");
-                connection.end();
-            break;
-
+    // prompting the options for the user
+    inquirer.prompt ([
+        {
+        type: "list",
+        massage: "Please select one option",
+        name: "action",
+        choices: [
+            "View all departments",
+            "View all roles",
+            "View employees",
+            "Add a department",
+            "Add a role",
+            "Add an employee",
+            "Update an employee information",
+            "View departmental spend on HR",
+            "Delete a department, role or employee",
+            "Exit One Team Manager Application"
+            ]
         }
-    })
+        ]).then(function(selection){
+            // creating switch based on the selected action
+            switch(selection.action) {
+                // viewAllDepartment function if "View all department" is selected
+                case "View all departments": 
+                    viewAllDepartments();
+                break;
+
+                // viewAllRoles function if "View all role" is selected
+                case "View all roles":
+                    viewAllRoles();
+                break;
+
+                // viewEmployees function if "View employees" is selected
+                case "View employees":
+                    viewEmployees();
+                break;
+
+                // departmentAdd function if "Add a department" is selected
+                case "Add a department":
+                    departmentAdd();
+                break;
+
+                // roleAdd function if "Add a role" is selected
+                case "Add a role":
+                    roleAdd();
+                break;
+
+                // employeeAdd function if "Add an employee" is selected
+                case "Add an employee":
+                    employeeAdd();
+                break;
+
+                // updateEmployee function if "Update an employee information" is selected
+                case "Update an employee information":
+                    updateEmployee();
+                break;
+
+                // viewSpend function if "View departmental spend on HR" is selected
+                case "View departmental spend on HR":
+                    viewSpend();
+                break;
+
+                // deleteInfo function if "Delete a department, role or employee" is selected
+                case "Delete a department, role or employee":
+                    deleteInfo();
+                break;
+
+                // exit from the application
+                case "Exit One Team Manager Application":
+                    console.log ("===========================================================");
+                    console.log ("");
+                    console.log ("   YOU HAVE SUCCESSFULLY LOGGED OUT FROM THE APPLICATION   ");
+                    console.log ("");
+                    console.log ("===========================================================");
+                    dbConnection.end();
+                break;
+            }
+        }
+    )
+};
+
+// viewAllDepartments function
+function viewAllDepartments() {
+    dbConnection.query("SELECT id AS Department_ID, name AS Department FROM departments",
+    function(err, res) {
+      if (err) throw err
+      console.log("")
+      console.log("*** LIST OF ALL DEPARTMENTS ***")
+      console.log("")
+      console.table(res)
+      manageOneTeam()
+    });
+};
+
+// viewAllRoles function
+function viewAllRoles() {
+    dbConnection.query("SELECT id AS roles.Role_ID, role_name AS Title, salary as FROM departments",
+    function(err, res) {
+      if (err) throw err
+      console.log("")
+      console.log("*** LIST OF ALL DEPARTMENTS ***")
+      console.log("")
+      console.table(res)
+      manageOneTeam()
+    });
 };
